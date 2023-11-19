0ff9319218b7d22a6489fed7e7d1f8f9bbe09ab7
Lucas Pereira <lucas.pereira@valemobi.com.br>
2023-11-19 00:21:52 -0300

    Subindo icones para utilização no projeto

---
+++ app/src/main/java/com/example/firstcompose/ui/theme/view/HomeScreen.kt
@@ -0,0 +1,35 @@
+package com.example.firstcompose.ui.theme.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun HomeScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)) {
+
+        Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+            Text(text = "Instagram", fontSize = 32.sp)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
