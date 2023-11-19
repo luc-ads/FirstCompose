d897d60694b956d5c28df9ed13d73ee3a3c14925
Lucas Pereira <lucas.pereira@valemobi.com.br>
2023-11-19 00:39:24 -0300

    Construção da Toolbar

--- app/src/main/java/com/example/firstcompose/ui/theme/view/HomeScreen.kt
+++ app/src/main/java/com/example/firstcompose/ui/theme/view/HomeScreen.kt
@@ -2,16 +2,11 @@ package com.example.firstcompose.ui.theme.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -22,9 +17,7 @@ fun HomeScreen() {
             .fillMaxSize()
             .background(Color.White)) {
 
-        Row(modifier = Modifier.padding(horizontal = 16.dp)) {
-            Text(text = "Instagram", fontSize = 32.sp)
-        }
+        InstagramToolbar()
     }
 }
 
